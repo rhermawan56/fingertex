0b580e9f069c6df59aa9acbeaa207877f6305b5c
rizky hermawan <rhermawan56@gmail.com>
2025-12-03 14:31:22 +0700

    Update V2.15

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -49,7 +49,7 @@ class Employee extends Model
         }
     }
 
-    public static function getDataEmployees(Request $request, $data = null)
+    public static function getDataEmployeesBackup(Request $request, $data = null)
     {
         self::loginApi();
         $now = date('Y_m_d');
@@ -186,6 +186,176 @@ class Employee extends Model
         }
     }
 
+    public static function getDataEmployees(Request $request, $data = null)
+    {
+        self::loginApi();
+        $now = date('Y_m_d');
+        $filename = "get_userid_list_{$now}.txt";
+
+        try {
+
+            foreach ($data as $k => $v) {
+                $dataSend = [
+                    "trans_id" => $k + 1,
+                    "cloud_id" => $v->cloud_id
+                ];
+
+                $response = Http::withToken(self::$JWTTOKEN)->post(self::$getAllPinUrl, $dataSend);
+                $response = $response->json();
+            }
+
+            $maxWait = 15;
+            $interval = 0.5;
+            $waited = 0;
+
+            while (!Storage::exists($filename) && $waited < $maxWait) {
+                usleep($interval * 1_000_000);
+                $waited += $interval;
+            }
+
+            $file = '';
+            $file = Storage::get($filename);
+            $lines = array_filter(array_map('trim', explode("\n", $file)));
+            $fileJson = [];
+            foreach ($lines as $line) {
+                $decoded = json_decode($line, true);
+                if ($decoded !== null) {
+                    $fileJson[] = $decoded;
+                }
+            }
+
+            $fileJsonUnique = collect($fileJson)
+                ->keyBy('cloud_id')
+                ->values()
+                ->toArray();
+
+            $pin = collect($fileJsonUnique)
+                ->pluck('data.pin_arr')
+                ->flatten()
+                // ->unique()
+                ->values()
+                ->toArray();
+
+            $raw = [];
+            $excludeFilter = ['machine', 'cloud_id'];
+            $columns = function () use ($request) {
+                $col = collect($request->input('columns'));
+
+                return $col->filter(function ($item) {
+                    return $item['search']['value'];
+                });
+            };
+
+            if ($columns()) {
+                foreach ($columns() as $k => $v) {
+                    $after = [];
+                    $before = [];
+                    $none = [];
+
+                    if (!in_array($v['data'], $excludeFilter)) {
+                        if (in_array($v['data'], $after)) {
+                            array_push($raw, "{$v['data']} like '{$v['search']['value']}%'");
+                        } else if (in_array($v['data'], $before)) {
+                            array_push($raw, "{$v['data']} like '%{$v['search']['value']}'");
+                        } else if (in_array($v['data'], $none)) {
+                            array_push($raw, "{$v['data']} like '{$v['search']['value']}'");
+                        } else {
+                            array_push($raw, "{$v['data']} like '%{$v['search']['value']}%'");
+                        }
+                    }
+                }
+            }
+
+            $dataSend = [
+                // 'wherein' => [
+                //     [
+                //         'field' => 'kar_idxx',
+                //         'values' => $pin
+                //     ]
+                // ],
+                "raw" => $raw,
+                "start" => $request->input('start'),
+                "length" => $request->input('length')
+            ];
+
+            if (count($pin) < 1000) {
+                $dataSend['wherein'][] = [
+                    'field' => 'kar_id',
+                    'values' => $pin
+                ];
+            }
+
+            $employeesData = [
+                "data" => [],
+                "rows" => 0
+            ];
+            $requestEmployee = Http::withToken(self::$JWTTOKEN)->post(self::$getEmployeeUrl, $dataSend);
+            $requestEmployee = $requestEmployee->json();
+            $employeesData['rows'] = $employeesData['rows'] + $requestEmployee['rows'];
+
+            $dataSend['company'] = 'SINAR TERANG';
+            $requestEmployeeSinter = Http::withToken(self::$JWTTOKEN)->post(self::$getEmployeeUrl, $dataSend);
+            $requestEmployeeSinter = $requestEmployeeSinter->json();
+            $employeesData['rows'] = $employeesData['rows'] + $requestEmployeeSinter['rows'];
+
+            foreach ($requestEmployee['data'] as $k => &$v) {
+                $cloudId = collect($fileJsonUnique)->filter(function ($item) use ($v) {
+                    return in_array($v['kar_id'], $item['data']['pin_arr']);
+                })->values()->pluck('cloud_id')->toArray();
+
+                $machine = collect($data)->filter(function ($item) use ($cloudId) {
+                    return in_array($item['cloud_id'], $cloudId);
+                })->values();
+
+                $v['cloud_id'] = $machine->map(function ($item) {
+                    return $item['cloud_id'];
+                });
+
+                $v['machine'] = $machine->map(function ($item) {
+                    return $item['msn_name'];
+                });
+
+                array_push($employeesData['data'], $v);
+            }
+
+            foreach ($requestEmployeeSinter['data'] as $k => &$v) {
+                $v['kar_id'] = "80{$v['kar_id']}";
+                $cloudId = collect($fileJsonUnique)->filter(function ($item) use ($v) {
+                    return in_array($v['kar_id'], $item['data']['pin_arr']);
+                })->values()->pluck('cloud_id')->toArray();
+
+                $machine = collect($data)->filter(function ($item) use ($cloudId) {
+                    return in_array($item['cloud_id'], $cloudId);
+                })->values();
+
+                $v['cloud_id'] = $machine->map(function ($item) {
+                    return $item['cloud_id'];
+                });
+
+                $v['machine'] = $machine->map(function ($item) {
+                    return $item['msn_name'];
+                });
+
+                array_push($employeesData['data'], $v);
+            }
+
+            return response()->json([
+                'status' => true,
+                'data' => $employeesData['data'],
+                'rows' => $employeesData['rows'],
+                'messages' => 'success',
+            ], 200);
+        } catch (\Exception $e) {
+
+            return response()->json([
+                'status' => false,
+                'data' => [],
+                'rows' => 0,
+                'messages' => $e->getMessage()
+            ], 400);
+        }
+    }
+
     public static function getDataEmployee($id, $data)
     {
         self::loginApi();
@@ -244,19 +414,35 @@ class Employee extends Model
                 ->values()
                 ->toArray();
 
+            $company = '';
+            $newPin = [];
+            foreach ($pin as $k => $v) {
+                if (substr($v, 0, 2) == 80) {
+                    $company = 'SINAR TERANG';
+                    $newPin[] = substr($v,2);
+                }
+            }
+
             $dataSend = [
                 'wherein' => [
                     [
                         'field' => 'kar_id',
-                        'values' => $pin
+                        'values' => $newPin
                     ]
                 ]
             ];
+
+            if ($company) {
+                $dataSend['company'] = $company;
+            }
 
             $requestEmployee = Http::withToken(self::$JWTTOKEN)->post(self::$getEmployeeUrl, $dataSend);
             $requestEmployee = $requestEmployee->json();
 
             foreach ($requestEmployee['data'] as $k => &$v) {
+                if ($company == 'SINAR TERANG') {
+                    $v['kar_id'] = "80{$v['kar_id']}";
+                }
                 $cloudId = collect($fileJsonUnique)->filter(function ($item) use ($v) {
                     return in_array($v['kar_id'], $item['data']['pin_arr']);
                 })->values()->pluck('cloud_id')->toArray();
